b10a987d6592780b189d7e039dc7b6369927992f
DN-Vanguard <Domiltannguyen@outlook.com>
2021-06-02 18:42:36 -0700

    completed questions in javascript, and have initial layout complete.

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+//Questions formed in an array
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers",
+        ],
+        correct: "alerts"
+    },
+
+    {
+        question: "The condition in an if / else statement is enclosed within ________.",
+        choices: [
+            "'' / quotes",
+            "{} / curly brackets",
+            "() / parentheses",
+            "[] / square brackets",
+        ],
+        correct: "() / parentheses"
+    },
+
+    {
+        question: "Arrays in Javascript can be used to store _______.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all the above",
+        ],
+        correct: "all the above"
+    },
+
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        choices: [
+            ", / commas",
+            "{} / curly brackets",
+            "'' / quotes",
+            "() / parentheses",
+        ],
+        correct: "'' / quotes"
+    },
+
+    {
+        question: "A very useful tool used during the devlopment and debugging for printing content to the debugger is:",
+        choices: [
+            "Javascript",
+            "terminal / bash",
+            "for loops",
+            "console log",
+        ],
+        correct: "console log"
+    },
+]
